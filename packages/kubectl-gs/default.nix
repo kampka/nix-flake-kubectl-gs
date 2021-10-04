a716f886d0a6718ba6690fb7ceec6a6c5c4dee99
Christian Kampka <christian@kampka.net>
2021-10-04 13:27:39 +0200

    Update to version 1.41.0

--- packages/kubectl-gs/default.nix
+++ packages/kubectl-gs/default.nix
@@ -2,9 +2,9 @@
 
 pkgs.buildGoModule rec {
   pname = "kubectl-gs";
-  version = "1.40.0";
+  version = "1.41.0";
 
-  vendorSha256 = "sha256-CEUVp824mzK6EeRty4l/EgHOwu6CwaSrOVZjEhjl3Co=";
+  vendorSha256 = "sha256-Dl6A2mkY/jsVGlzSu3LVK0DcggQpbqhzv4OIsPfIWvg=";
 
   doCheck = false;
 
@@ -12,7 +12,7 @@ pkgs.buildGoModule rec {
     owner = "giantswarm";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-RzU7HI6cpZDsMKxJL/3KmxlaISJgEN/kvwnSczIxQ1o=";
+    sha256 = "sha256-kB+Hz2JpSgplbxHFvDWabGPZPY2fhLZo6BUSEmROy3c=";
   };
 
   meta = with pkgs.lib; {
