f005f9b5cd242aa2a857733e99ce4549ee4cf44b
Christian Kampka <christian@kampka.net>
2021-11-12 11:29:25 +0100

    Update to version 1.48.1

--- packages/kubectl-gs/default.nix
+++ packages/kubectl-gs/default.nix
@@ -1,19 +1,25 @@
-{ pkgs }:
+{ pkgs, makeWrapper, ... }:
 
 pkgs.buildGoModule rec {
   pname = "kubectl-gs";
-  version = "1.46.0";
+  version = "1.48.1";
 
-  vendorSha256 = "sha256-HGA5fsT7/I6z2EnXikHmAMqlDzcn6cQDnwEezCakkFs=";
+  vendorSha256 = "sha256-OOd0fjsdjBL01Jvp1/ArFTKIaU8zP4C5L8OLJMgJb1s=";
 
   doCheck = false;
+
+  nativeBuildInputs = [ makeWrapper ];
 
   src = pkgs.fetchFromGitHub {
     owner = "giantswarm";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-jn/Yq0ee8Dgr1j5XWt3PBvALPywgf9HcZGyqfwRygmU=";
+    sha256 = "sha256-FN50QSEoeXwC8dJWDz9kSesYb0lwf+2s14AoScFw98s=";
   };
+
+  postFixup = ''
+    wrapProgram $out/bin/kubectl-gs --add-flags "--disable-version-check"
+  '';
 
   meta = with pkgs.lib; {
     description = "The official Giant Swarm kubectl plug-in";
