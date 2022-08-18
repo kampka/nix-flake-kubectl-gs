9bb99dd7332ff6e65a400e099186c2557c106e13
Christian Kampka <christian@kampka.net>
2022-08-18 10:04:05 +0200

    Update version to 2.19.2

--- packages/kubectl-gs/default.nix
+++ packages/kubectl-gs/default.nix
@@ -2,7 +2,7 @@
 
 pkgs.buildGoModule rec {
   pname = "kubectl-gs";
-  version = "2.19.1";
+  version = "2.19.2";
 
   vendorSha256 = "sha256-gpYyBCs3sUuZ3PEP3rtmwn0MJYQ8DooqBncuqPcq5fs=";
 
@@ -14,7 +14,7 @@ pkgs.buildGoModule rec {
     owner = "giantswarm";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-OdeU187bO2LlMVWY2CGjQuGAE4/6URLEuDW1A9pVUdQ=";
+    sha256 = "sha256-DHv+T+gbO9C8DfpdRory6e9Drw7P+g6N1LAZtbJYcHw=";
   };
 
   postFixup = ''
